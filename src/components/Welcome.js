2312eb2a93114dc9f88b23ee1a71e713a997540a
Manishkmr406 <manishkmr406@gmail.com>
2023-02-09 16:33:13 +0530

    added

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -2,7 +2,8 @@
 function Welcome(props){
     return(
         <>
-          <h1>Hey!{props.name}</h1>
+          Hey !
+          <h1>{props.name}</h1>
           <h2>Welcome to Newton School.</h2>
         </>
     );
