ed5b6f1f05291b9c083e9c3ea21b4aaca514b24f
Manishkmr406 <manishkmr406@gmail.com>
2023-02-09 16:49:16 +0530

    update

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -2,10 +2,10 @@
 function Welcome(props){
     return(
         <>
-          Hey !
-          <h1>{props.name}</h1>
+          <h1>Hey !{props.name}</h1>
           <h2>Welcome to Newton School.</h2>
         </>
-    );
+    )
 }
+
 export default Welcome;
